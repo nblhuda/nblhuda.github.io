cd596683fac2aea600f83930df8a721fd5c9957b
NABILA HUDA BINTI SUHAIMI <68523742+nblhuda@users.noreply.github.com>
2020-09-08 23:13:20 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,40 @@
+const main = {
+  name: 'Nabila Huda Suhaimi',
+  mail: 'bylasuhaimi@gmail.com',
+  img: 'https://scontent.fkul16-1.fna.fbcdn.net/v/t1.0-9/82237573_2927478213987328_7049825462150758400_n.jpg?_nc_cat=100&_nc_sid=09cbfe&_nc_ohc=vk3f1PJWss8AX9SepCB&_nc_ht=scontent.fkul16-1.fna&oh=cd0647621421471f261daf589dbce993&oe=5F7D885C',
+  role: ['Programmer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Home',
+      iconName: 'paper-plane-outline',
+      link: 'https://t.me/nabilahudasuhaimi'
+    },
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/nblhuda'
+    },
+    {
+      name: 'Instagram',
+      iconName: 'logo-instagram',
+      link: 'https://www.instagram.com/nbl.huda/?hl=ms'
+    },
+    {
+      name: 'Facebook',
+      iconName: 'logo-facebook',
+      link: 'https://www.facebook.com/nabila.suhaimi.7/'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'bylasuhaimi@gmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: '(LEMP stack)', link: '#LEMP' },
+    { name: 'Laravel', link: '#Laravel' },
+    { name: 'Flutter', link: '#Flutter' },
+    { name: 'Ionic', link: '#Ionic' }
+  ]
+};
